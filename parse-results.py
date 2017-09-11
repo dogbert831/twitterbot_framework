d89498d41a3a95f831b9e591fb2a94b2f4e1fe07
dogbert831 <miclarke@nwcybernet.com>
2017-09-11 20:34:58 +0000

    file clean up

--- parse-results.py
+++
@@ -1,6 +0,0 @@
-with open("results.txt") as f:
-    for line in f:
-        if "trump" in line:
-        	lastmatch = line
-    if lastmatch is not None:
-    	print(lastmatch)
--- test-tweet.py
+++
@@ -1,7 +0,0 @@
-import guardian_content
-
-ttweet = guardian_content.scrape_guardian()
-tweet = ttweet[0]
-url = ttweet[1]
-text = tweet + " " + url
-print(text)
